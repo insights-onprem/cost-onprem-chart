c0f019ac249bd1dc6bd323c6a8d1f8702f959cd4
Thomas Stetson <tstetson@redhat.com>
2026-05-12 15:55:28 -0400

    Remove temporary deeper testing flag from recommend-tests script. This change eliminates the forced deeper testing configuration that was previously used for validation purposes, streamlining the testing process.
    Signed-off-by: Thomas Stetson <tstetson@redhat.com>

--- scripts/qe/recommend-tests.sh
+++ scripts/qe/recommend-tests.sh
@@ -320,10 +320,6 @@ if [[ "$max_profile" != "smoke" ]]; then
     needs_deeper="true"
 fi
 
-# TODO: TEMPORARY - force deeper testing to validate workflow fix (remove after testing)
-needs_deeper="true"
-max_profile="${max_profile:-extended}"
-
 output_var "suggested_profile" "$max_profile"
 output_var "needs_deeper_testing" "$needs_deeper"
 
